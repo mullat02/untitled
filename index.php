10eaf10dbf230f152182a78e59c8b19c33e22bcd
mullat02 <62921577+mullat02@users.noreply.github.com>
2020-04-13 12:57:13 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Mulla Tasnim
- * Date: 19/10/2019
- * Time: 22:28
- * Purpose:
- */
+
 
 if(!isset($_SESSION))
 {
     session_start();
 }
 
-$page_title = 'Home - Quality Books';
+$page_title = 'Home - Online Booking System';
 $page_content = 'Home.php';
 
 include ('structure/Main.php');
